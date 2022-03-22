b1dba03949c3b8de71e880bcd927f3ad5d594895
Foroni19 <90644344+Foroni19@users.noreply.github.com>
2022-03-22 18:39:42 -0300

    Create codigo.ino

---
+++ ex2/codigo.ino
@@ -0,0 +1,30 @@
+const int buttonPin = 2;
+const int ledPin = 13;
+
+
+int buttonState = 0;
+
+void setup() {
+  
+ pinMode (ledPin, OUTPUT);
+  
+  
+  pinMode(2, INPUT);
+}
+  
+void loop()  {
+   
+   buttonState = digitalRead(2);
+   
+   
+   if (buttonState == HIGH)
+   {
+   //
+   digitalWrite(13, HIGH);
+   delay(1000);
+   digitalWrite(13, LOW);
+   delay(1000);
+   
+   }
+}
+     
